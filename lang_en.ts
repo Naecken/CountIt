a767be820c67114194133db164272311751abd7c
Naecken <naecken@ovi.com>
2015-01-12 07:42:01 +0100

    Create lang_en.ts

---
+++ lang_en.ts
@@ -0,0 +1,493 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en" sourcelanguage="en">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="64"/>
+        <source>CountIt</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="73"/>
+        <source>Developed by</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="91"/>
+        <source>Version</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="117"/>
+        <source>Cooperates with</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="153"/>
+        <source>Powered by</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="219"/>
+        <source>Credits</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="227"/>
+        <source>Allstar12345 for his patience</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="234"/>
+        <source>Sharing detail</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="242"/>
+        <source>Symbian Store release.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="254"/>
+        <source>Applist release.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="261"/>
+        <source>About application</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="270"/>
+        <source>CountIt is counter application based on QML. It&apos;s compatible with all Symbian phones with Symbian Anna and newer version of Symbian.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="283"/>
+        <source>Contact me</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="312"/>
+        <source>About</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AboutPage.qml" line="315"/>
+        <source>Yeah, page about this app.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>AppMenu</name>
+    <message>
+        <location filename="../qml/Tapper/AppMenu.qml" line="11"/>
+        <source>Quit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AppMenu.qml" line="16"/>
+        <source>Minimize</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AppMenu.qml" line="21"/>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AppMenu.qml" line="23"/>
+        <source>If is proximity sensor enabled, Counter can works in background.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/AppMenu.qml" line="27"/>
+        <source>Disable sounds</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>CountDown</name>
+    <message>
+        <location filename="../qml/Tapper/CountDown.qml" line="16"/>
+        <source>I count it via CountIt and </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountDown.qml" line="16"/>
+        <source> left from </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountDown.qml" line="19"/>
+        <source> still left</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountDown.qml" line="47"/>
+        <location filename="../qml/Tapper/CountDown.qml" line="60"/>
+        <location filename="../qml/Tapper/CountDown.qml" line="74"/>
+        <location filename="../qml/Tapper/CountDown.qml" line="103"/>
+        <location filename="../qml/Tapper/CountDown.qml" line="131"/>
+        <location filename="../qml/Tapper/CountDown.qml" line="146"/>
+        <location filename="../qml/Tapper/CountDown.qml" line="183"/>
+        <source>Only left </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountDown.qml" line="51"/>
+        <location filename="../qml/Tapper/CountDown.qml" line="109"/>
+        <location filename="../qml/Tapper/CountDown.qml" line="150"/>
+        <source>0 remains from </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountDown.qml" line="104"/>
+        <source>Proximity sensor detection!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountDown.qml" line="177"/>
+        <source>In clipboard is saved this text: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountDown.qml" line="182"/>
+        <source>Make a long press to reset counter.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountDown.qml" line="190"/>
+        <source>Count Down</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountDown.qml" line="193"/>
+        <source> This page allows you count a specific number between 1-999 999. Tap on screen to count down or use proximity sensor. To count up make a long press on screen. If application reaches 0, the gong will informates you.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>CountUp</name>
+    <message>
+        <location filename="../qml/Tapper/CountUp.qml" line="15"/>
+        <source>I count it via CountIt and result is </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountUp.qml" line="58"/>
+        <location filename="../qml/Tapper/CountUp.qml" line="113"/>
+        <source>Ready to start</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountUp.qml" line="76"/>
+        <source>Proximity sensor detection!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountUp.qml" line="107"/>
+        <source>In clipboard is saved this text: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountUp.qml" line="112"/>
+        <source>Make a long press to reset counter.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountUp.qml" line="142"/>
+        <source>Count Up</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/CountUp.qml" line="145"/>
+        <source>Tap on screen or place hand near to proximity sensor on the top of your phone. To count down make a long press on screen.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/Tapper/MainPage.qml" line="29"/>
+        <source>Count Up</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/MainPage.qml" line="52"/>
+        <location filename="../qml/Tapper/MainPage.qml" line="278"/>
+        <source>MultiCount Up</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/MainPage.qml" line="75"/>
+        <location filename="../qml/Tapper/MainPage.qml" line="221"/>
+        <source>Count Down</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/MainPage.qml" line="99"/>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/MainPage.qml" line="123"/>
+        <source>About</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/MainPage.qml" line="147"/>
+        <source>Last counts - temporarily saved</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/MainPage.qml" line="173"/>
+        <source>Count up</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/MainPage.qml" line="181"/>
+        <location filename="../qml/Tapper/MainPage.qml" line="229"/>
+        <location filename="../qml/Tapper/MainPage.qml" line="286"/>
+        <source>Ready to continue</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/MainPage.qml" line="308"/>
+        <source>Help</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/MainPage.qml" line="318"/>
+        <source>In every header is hidden information about actual page. Also with long press on header you can open quick menu everywhere.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>PreCountDownII</name>
+    <message>
+        <location filename="../qml/Tapper/PreCountDownII.qml" line="25"/>
+        <source>Enter value here</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/PreCountDownII.qml" line="36"/>
+        <source>Clear</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/PreCountDownII.qml" line="43"/>
+        <location filename="../qml/Tapper/PreCountDownII.qml" line="69"/>
+        <source>Continue</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/PreCountDownII.qml" line="80"/>
+        <source>Count Down</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/PreCountDownII.qml" line="83"/>
+        <source> This page allows you count a specific number between 1-999 999. At first set value how many signals you want count. After that press continue button.</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="32"/>
+        <source>Sound settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="41"/>
+        <source>Volume of alarm </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="75"/>
+        <location filename="../qml/Tapper/Settings.qml" line="150"/>
+        <source>Already set</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="78"/>
+        <location filename="../qml/Tapper/Settings.qml" line="91"/>
+        <location filename="../qml/Tapper/Settings.qml" line="153"/>
+        <source>Save volume as: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="105"/>
+        <source>Volume of sensor sound </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="160"/>
+        <source>Control settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="168"/>
+        <source>Proximity sensor enabled</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="177"/>
+        <source>Allows nontouch control. Proximity sensor is little bit slower - reaction time is something around 250ms.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="191"/>
+        <source>Proximity sensor is located on the top of screen next to the phone speaker.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="228"/>
+        <source>Works in background </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="238"/>
+        <source>Allows nontouch control when phone is locked or application is minimized.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="264"/>
+        <source>Start settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="280"/>
+        <source>Settings first next time</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="305"/>
+        <source>Settings first everytime</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="367"/>
+        <source>All sounds are set to 0. </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="372"/>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/Settings.qml" line="375"/>
+        <source>Settings page... What more I can say?</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>multiCount</name>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="16"/>
+        <source>I count it via CountIt and result(s) is(are) </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="55"/>
+        <source>Add counter</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="55"/>
+        <source>Count first (-_-)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="272"/>
+        <source>You must count something first.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="275"/>
+        <source>I count it via CountIt and result is </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="276"/>
+        <source>I count it via CountIt and results are: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="377"/>
+        <source>Red</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="386"/>
+        <source>Blue</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="394"/>
+        <source>Green</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="402"/>
+        <source>Purple</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="411"/>
+        <source>Yellow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="419"/>
+        <source>Orange</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="427"/>
+        <source>White</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="461"/>
+        <location filename="../qml/Tapper/multiCount.qml" line="481"/>
+        <location filename="../qml/Tapper/multiCount.qml" line="501"/>
+        <location filename="../qml/Tapper/multiCount.qml" line="521"/>
+        <location filename="../qml/Tapper/multiCount.qml" line="540"/>
+        <source>I count it via CountIt and result is: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="461"/>
+        <location filename="../qml/Tapper/multiCount.qml" line="481"/>
+        <location filename="../qml/Tapper/multiCount.qml" line="501"/>
+        <location filename="../qml/Tapper/multiCount.qml" line="521"/>
+        <location filename="../qml/Tapper/multiCount.qml" line="540"/>
+        <source>In clipboard is saved this text: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="563"/>
+        <source>MultiCount Up</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="565"/>
+        <source>Press edit button and add new counters. You can also delete them or reset specific counter.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="584"/>
+        <source>Give me a name</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/Tapper/multiCount.qml" line="617"/>
+        <source>Clear</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
